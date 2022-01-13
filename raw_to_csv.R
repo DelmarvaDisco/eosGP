c3bc755c02db6ed694269bafae99f364adee7941
James Maze <jtmaze@vt.edu>
2022-01-13 14:57:37 -0500

    - Progress

--- raw_to_csv.R
+++ raw_to_csv.R
@@ -6,7 +6,9 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #Notes:
-#   -
+#   - Because Campbell loggers appends new downloads to the previous file path, 
+#     we need to remove overlap in the data frames
+#   - Should we remove data where the sensors were dry?
 
 
 # 1. Libraries and workspace ----------------------------------------------
@@ -18,7 +20,7 @@ library(lubridate)
 library(tidyverse)
 library(stringr)
 
-source("functions/download_fun")
+source("functions/download_fun.R")
 
 data_dir <- "data/2021/"
 
@@ -33,7 +35,37 @@ data <- eosGP_files %>%
   reduce(rbind)
 
 
-# 3. Concatenate files by site --------------------------------------------
+# 3. Remove the redundant data points and bad measurement times -----------------------------------------------
+
+data <- data %>% 
+  distinct(Site_ID, TIMESTAMP, RECORD, .keep_all = TRUE) %>% 
+  filter(!TIMESTAMP == "TS") %>% #Removes the wonky rows below column names
+  transmute(Timestamp = ymd_hms(TIMESTAMP),
+         BattV = as.numeric(BattV),
+         Logger_TempC = as.numeric(PTemp_C),
+         CO2_Conc_ppm = as.numeric(GP_CO2Conc),
+         CO2_HiConc_ppm = as.numeric(GP_CO2HiConc),
+         GP_TempC = as.numeric(GP_Temp),
+         Site_ID = Site_ID,
+         file = file) #Remove this column once data is clean 
+  
+
+#   3a. DK-SW cleaning ------------------------------------------------------
+
+data_DK <- data %>% 
+  filter(Site_ID == "DK_SW") 
+
+#   3b. TS-SW cleaning ------------------------------------------------------
+
+data_TS <- data %>% 
+  filter(Site_ID == "TS_SW") 
+
+#   3c. ND-SW cleaning ------------------------------------------------------
+
+data_ND <- data %>% 
+  filter(Site_ID == "ND_SW") 
+
+
 
 
 # 4. Convert time zone EDT to EST ---------------------------------------------------
