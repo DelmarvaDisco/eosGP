b3b2cc6c680ed8b8a7f1f9a0600d094fd59451ae
James Maze <jtmaze@vt.edu>
2022-01-13 13:12:26 -0500

    Big tibble!

--- raw_to_csv.R
+++ raw_to_csv.R
@@ -5,21 +5,43 @@
 # Purpose: Aggregate the raw downloads into a clean .csv
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-# 1. Read the JL files ----------------------------------------------
-
-
+#Notes:
+#   -
 
 
-# 2. Concatenate files by site --------------------------------------------
+# 1. Libraries and workspace ----------------------------------------------
+
+remove(list = ls())
+
+library(purrr)
+library(lubridate)
+library(tidyverse)
+library(stringr)
+
+source("functions/download_fun")
+
+data_dir <- "data/2021/"
+
+# 2. Read the JL files ----------------------------------------------
+
+eosGP_files <- list.files(paste0(data_dir), full.names = TRUE)
+
+eosGP_files <- eosGP_files[str_detect(eosGP_files, "eosGP")]
+
+data <- eosGP_files %>% 
+  map(download_fun) %>% 
+  reduce(rbind)
 
 
-# 3. Convert time zone EDT to EST ---------------------------------------------------
+# 3. Concatenate files by site --------------------------------------------
 
 
-# 4. Read the BC files -----------------------------------------------------
+# 4. Convert time zone EDT to EST ---------------------------------------------------
 
 
-# 5. Combine into one large .csv ------------------------------------------
+# 5. Read the BC files -----------------------------------------------------
+
+
+# 6. Combine into one large .csv ------------------------------------------
 
 
